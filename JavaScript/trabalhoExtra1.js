4f3a84c6fa57fd1154524d44777c08160259c23d
Rodrigo Souza <rodrigo.lopessouza@hotmail.com>
2023-11-13 15:19:32 -0300

    ativ extra 1 de js

---
+++ JavaScript/trabalhoExtra1.js
@@ -0,0 +1,21 @@
+// Definindo as variáveis
+var nome = "João";
+var salario = 3000;
+var idade = 30;
+var possuiDiploma = true;
+
+// Exibindo os tipos de dados
+console.log("Tipo de 'nome': " + typeof nome);
+console.log("Tipo de 'salario': " + typeof salario);
+console.log("Tipo de 'idade': " + typeof idade);
+console.log("Tipo de 'possuiDiploma': " + typeof possuiDiploma);
+
+// Operações sugeridas
+var novoSalario = salario + 500; // Aumento de salário
+var proximoAno = idade + 1; // Aniversário
+var statusDiploma = possuiDiploma ? "A pessoa possui diploma" : "A pessoa não possui diploma"; // Verificação de diploma
+
+// Exibindo os resultados das operações
+console.log("Novo salário: " + novoSalario);
+console.log("Idade no próximo ano: " + proximoAno);
+console.log("Status do diploma: " + statusDiploma);
